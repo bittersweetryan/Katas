d704fd488665847d35cb1387ac78fe0a58c81066
Ryan Anklam <ryan.anklam@gmail.com>
2011-10-10 08:19:23 -0500

    first karate chop in js

--- KarateChop/karateChop.js
+++ KarateChop/karateChop.js
@@ -1,7 +1,42 @@
-function chop(int, theArray){
-	//first make sure the array is an array
-	console.log(Object.prototype.toString.call(theArray));
-	if(!theArray || Object.prototype.toString.call(theArray) !== "[object Array]"){
-		ok(false,"Not an array");
+function chop(target, theArray){
+	var mid,
+		min,
+		max,
+		len;
+	
+	//first make sure the array is an array, it has a length, and that target is a number
+	if(!theArray || 
+		Object.prototype.toString.call(theArray) !== "[object Array]" || 
+		!theArray.length || 
+		isNaN(parseInt(target,10))) {
+			console.log("not an array");
+			return -1;
 	}
+	
+	len = theArray.length;
+	min = 0;
+	max = theArray.length - 1;
+	mid = Math.floor(max / 2);
+
+	if(theArray[mid] === target){
+		return mid;
+	}
+	
+	while(min < max){
+		//if the target is less than the array's middle set the new max to the middle, keep the min the same
+		if(target < theArray[mid]){
+			max = mid - 1;
+		}
+		else{
+			min = mid + 1;
+		}	
+		
+		mid = Math.floor((min + max) / 2);
+		
+		if(theArray[mid] === target){
+			return mid+1;
+		}
+	}
+	
+	return -1;
 };
--- KarateChop/karateChopTest.js
+++ KarateChop/karateChopTest.js
@@ -1,3 +1,24 @@
-test("Chop1", function(){
-	chop(1,[4,9,32,3,7]);
+test("Chop1ShouldReturn1", function(){
+	var expected = -1;
+	//the array passed into chop is assumed to be sorted
+	var actual = chop(1,[3,4,7,9,32]);
+	
+	equal(expected,actual);
+});
+
+
+test("Chop4ShouldReturn2", function(){
+	var expected = 2;
+	//the array passed into chop is assumed to be sorted
+	var actual = chop(4,[3,4,7,9,32]);
+	
+	equal(actual,expected);
+});
+
+test("Chop9ShouldReturn4", function(){
+	var expected = 5;
+	//the array passed into chop is assumed to be sorted
+	var actual = chop(32,[3,4,7,9,32]);
+	
+	equal(actual,expected);
 });
